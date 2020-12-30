2605cc9d0f75cddfc2f3714d0f7b6843bc499ed8
Siydulislam <siydulislam333@gmail.com>
2020-12-30 14:06:35 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import OrderReview from './Components/OrderReview/OrderReview';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/shop" component={Home}></Route>
+            <Route path="/review" component={OrderReview}></Route>
+            <Route exact path="*" component={NotFound}></Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
